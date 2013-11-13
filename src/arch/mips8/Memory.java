9ecc4caa51b242378b15831a8c4a2d59d3a36bce
Tanmay Randhavane <tvrandhavane@gmail.com>
2013-11-14 02:25:30 +0530

    Basic Memory class functions

--- src/arch/mips8/Memory.java
+++ src/arch/mips8/Memory.java
@@ -1,11 +1,26 @@
 package arch.mips8;
 
 public class Memory {
-
+	MemoryDisk dataMemory;
+	MemoryDisk stackMemory;
+	
 	public Memory() {
 		// TODO Auto-generated constructor stub
-		MemoryDisk dataMemory = new MemoryDisk();
-		MemoryDisk stackMemory = new MemoryDisk();
+		dataMemory = new MemoryDisk();
+		stackMemory = new MemoryDisk();
 	}
 
+	public int getWord(int index){
+		return dataMemory.getInt(index);
+	}
+	
+	public int getHalfWord(int index){
+		return dataMemory.getHalfword(index);
+	}
+	
+	public int getByte(int index){
+		return dataMemory.getByte(index);
+	}
+	
+	
 }
