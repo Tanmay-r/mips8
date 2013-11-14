40a8a25c9093e0ea2e4211e6e1429fa0439f45eb
Tanmay Randhavane <tvrandhavane@gmail.com>
2013-11-14 23:56:31 +0530

    small change in try1.asm file

--- codes/try1.asm
+++ codes/try1.asm
@@ -1,11 +1,11 @@
 .text
 main:
-lui $t1, 35
-sw $t1, 0($t0)
-lw $t2, 0($t0)
-add $t4,$t5,$t1
-add $t7,$t7,$t1
-beq $t1, $t2, -2
-sltu $t4,$t5,$t1
-sub $t4,$t5,$t1
-and $t7,$t5,$t1
+		lui $t1, 35
+		sw $t1, 0($t0)
+		lw $t2, 0($t0)
+label:	add $t4,$t5,$t1
+		add $t7,$t7,$t1
+		beq $t1, $t2, label
+		sltu $t4,$t5,$t1
+		sub $t4,$t5,$t1
+		and $t7,$t5,$t1
